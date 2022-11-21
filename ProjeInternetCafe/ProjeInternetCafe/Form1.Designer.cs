c5df9e83be99efe91c8d157f9d19e4833f982dfd
Ahmet Ok <ahmetok2003@gmail.com>
2022-11-21 21:39:31 +0300

    v1.4

---
+++ ProjeInternetCafe/ProjeInternetCafe/Form1.Designer.cs
@@ -0,0 +1,390 @@
+﻿namespace ProjeInternetCafe
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.masa1 = new System.Windows.Forms.Button();
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.masa2 = new System.Windows.Forms.Button();
+            this.masa3 = new System.Windows.Forms.Button();
+            this.masa4 = new System.Windows.Forms.Button();
+            this.masa5 = new System.Windows.Forms.Button();
+            this.masa6 = new System.Windows.Forms.Button();
+            this.masa7 = new System.Windows.Forms.Button();
+            this.masa8 = new System.Windows.Forms.Button();
+            this.masa9 = new System.Windows.Forms.Button();
+            this.masa18 = new System.Windows.Forms.Button();
+            this.masa17 = new System.Windows.Forms.Button();
+            this.masa16 = new System.Windows.Forms.Button();
+            this.masa15 = new System.Windows.Forms.Button();
+            this.masa14 = new System.Windows.Forms.Button();
+            this.masa13 = new System.Windows.Forms.Button();
+            this.masa12 = new System.Windows.Forms.Button();
+            this.masa11 = new System.Windows.Forms.Button();
+            this.masa10 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // masa1
+            // 
+            this.masa1.FlatAppearance.BorderSize = 0;
+            this.masa1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.masa1.ImageKey = "pngwing.com (2).png";
+            this.masa1.ImageList = this.ımageList1;
+            this.masa1.Location = new System.Drawing.Point(12, 29);
+            this.masa1.Name = "masa1";
+            this.masa1.Size = new System.Drawing.Size(116, 116);
+            this.masa1.TabIndex = 0;
+            this.masa1.Text = "Masa-1";
+            this.masa1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.masa1.UseVisualStyleBackColor = true;
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.ımageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList1.ImageStream")));
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList1.Images.SetKeyName(0, "pngwing.com (2).png");
+            // 
+            // masa2
+            // 
+            this.masa2.FlatAppearance.BorderSize = 0;
+            this.masa2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.masa2.ImageKey = "pngwing.com (2).png";
+            this.masa2.ImageList = this.ımageList1;
+            this.masa2.Location = new System.Drawing.Point(134, 29);
+            this.masa2.Name = "masa2";
+            this.masa2.Size = new System.Drawing.Size(116, 116);
+            this.masa2.TabIndex = 1;
+            this.masa2.Text = "Masa-2";
+            this.masa2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.masa2.UseVisualStyleBackColor = true;
+            // 
+            // masa3
+            // 
+            this.masa3.FlatAppearance.BorderSize = 0;
+            this.masa3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.masa3.ImageKey = "pngwing.com (2).png";
+            this.masa3.ImageList = this.ımageList1;
+            this.masa3.Location = new System.Drawing.Point(256, 29);
+            this.masa3.Name = "masa3";
+            this.masa3.Size = new System.Drawing.Size(116, 116);
+            this.masa3.TabIndex = 2;
+            this.masa3.Text = "Masa-3";
+            this.masa3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.masa3.UseVisualStyleBackColor = true;
+            // 
+            // masa4
+            // 
+            this.masa4.FlatAppearance.BorderSize = 0;
+            this.masa4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.masa4.ImageKey = "pngwing.com (2).png";
+            this.masa4.ImageList = this.ımageList1;
+            this.masa4.Location = new System.Drawing.Point(378, 29);
+            this.masa4.Name = "masa4";
+            this.masa4.Size = new System.Drawing.Size(116, 116);
+            this.masa4.TabIndex = 3;
+            this.masa4.Text = "Masa-4";
+            this.masa4.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.masa4.UseVisualStyleBackColor = true;
+            // 
+            // masa5
+            // 
+            this.masa5.FlatAppearance.BorderSize = 0;
+            this.masa5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.masa5.ImageKey = "pngwing.com (2).png";
+            this.masa5.ImageList = this.ımageList1;
+            this.masa5.Location = new System.Drawing.Point(500, 29);
+            this.masa5.Name = "masa5";
+            this.masa5.Size = new System.Drawing.Size(116, 116);
+            this.masa5.TabIndex = 4;
+            this.masa5.Text = "Masa-5";
+            this.masa5.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.masa5.UseVisualStyleBackColor = true;
+            // 
+            // masa6
+            // 
+            this.masa6.FlatAppearance.BorderSize = 0;
+            this.masa6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.masa6.ImageKey = "pngwing.com (2).png";
+            this.masa6.ImageList = this.ımageList1;
+            this.masa6.Location = new System.Drawing.Point(622, 29);
+            this.masa6.Name = "masa6";
+            this.masa6.Size = new System.Drawing.Size(116, 116);
+            this.masa6.TabIndex = 5;
+            this.masa6.Text = "Masa-6";
+            this.masa6.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.masa6.UseVisualStyleBackColor = true;
+            // 
+            // masa7
+            // 
+            this.masa7.FlatAppearance.BorderSize = 0;
+            this.masa7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.masa7.ImageKey = "pngwing.com (2).png";
+            this.masa7.ImageList = this.ımageList1;
+            this.masa7.Location = new System.Drawing.Point(744, 29);
+            this.masa7.Name = "masa7";
+            this.masa7.Size = new System.Drawing.Size(116, 116);
+            this.masa7.TabIndex = 6;
+            this.masa7.Text = "Masa-7";
+            this.masa7.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.masa7.UseVisualStyleBackColor = true;
+            // 
+            // masa8
+            // 
+            this.masa8.FlatAppearance.BorderSize = 0;
+            this.masa8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.masa8.ImageKey = "pngwing.com (2).png";
+            this.masa8.ImageList = this.ımageList1;
+            this.masa8.Location = new System.Drawing.Point(866, 29);
+            this.masa8.Name = "masa8";
+            this.masa8.Size = new System.Drawing.Size(116, 116);
+            this.masa8.TabIndex = 7;
+            this.masa8.Text = "Masa-8";
+            this.masa8.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.masa8.UseVisualStyleBackColor = true;
+            // 
+            // masa9
+            // 
+            this.masa9.FlatAppearance.BorderSize = 0;
+            this.masa9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.masa9.ImageKey = "pngwing.com (2).png";
+            this.masa9.ImageList = this.ımageList1;
+            this.masa9.Location = new System.Drawing.Point(988, 29);
+            this.masa9.Name = "masa9";
+            this.masa9.Size = new System.Drawing.Size(116, 116);
+            this.masa9.TabIndex = 8;
+            this.masa9.Text = "Masa-9";
+            this.masa9.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.masa9.UseVisualStyleBackColor = true;
+            // 
+            // masa18
+            // 
+            this.masa18.FlatAppearance.BorderSize = 0;
+            this.masa18.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.masa18.ImageKey = "pngwing.com (2).png";
+            this.masa18.ImageList = this.ımageList1;
+            this.masa18.Location = new System.Drawing.Point(988, 146);
+            this.masa18.Name = "masa18";
+            this.masa18.Size = new System.Drawing.Size(116, 116);
+            this.masa18.TabIndex = 17;
+            this.masa18.Text = "Masa-18";
+            this.masa18.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.masa18.UseVisualStyleBackColor = true;
+            // 
+            // masa17
+            // 
+            this.masa17.FlatAppearance.BorderSize = 0;
+            this.masa17.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.masa17.ImageKey = "pngwing.com (2).png";
+            this.masa17.ImageList = this.ımageList1;
+            this.masa17.Location = new System.Drawing.Point(866, 146);
+            this.masa17.Name = "masa17";
+            this.masa17.Size = new System.Drawing.Size(116, 116);
+            this.masa17.TabIndex = 16;
+            this.masa17.Text = "Masa-17";
+            this.masa17.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.masa17.UseVisualStyleBackColor = true;
+            // 
+            // masa16
+            // 
+            this.masa16.FlatAppearance.BorderSize = 0;
+            this.masa16.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.masa16.ImageKey = "pngwing.com (2).png";
+            this.masa16.ImageList = this.ımageList1;
+            this.masa16.Location = new System.Drawing.Point(744, 146);
+            this.masa16.Name = "masa16";
+            this.masa16.Size = new System.Drawing.Size(116, 116);
+            this.masa16.TabIndex = 15;
+            this.masa16.Text = "Masa-16";
+            this.masa16.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.masa16.UseVisualStyleBackColor = true;
+            // 
+            // masa15
+            // 
+            this.masa15.FlatAppearance.BorderSize = 0;
+            this.masa15.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.masa15.ImageKey = "pngwing.com (2).png";
+            this.masa15.ImageList = this.ımageList1;
+            this.masa15.Location = new System.Drawing.Point(622, 146);
+            this.masa15.Name = "masa15";
+            this.masa15.Size = new System.Drawing.Size(116, 116);
+            this.masa15.TabIndex = 14;
+            this.masa15.Text = "Masa-15";
+            this.masa15.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.masa15.UseVisualStyleBackColor = true;
+            // 
+            // masa14
+            // 
+            this.masa14.FlatAppearance.BorderSize = 0;
+            this.masa14.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.masa14.ImageKey = "pngwing.com (2).png";
+            this.masa14.ImageList = this.ımageList1;
+            this.masa14.Location = new System.Drawing.Point(500, 146);
+            this.masa14.Name = "masa14";
+            this.masa14.Size = new System.Drawing.Size(116, 116);
+            this.masa14.TabIndex = 13;
+            this.masa14.Text = "Masa-14";
+            this.masa14.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.masa14.UseVisualStyleBackColor = true;
+            // 
+            // masa13
+            // 
+            this.masa13.FlatAppearance.BorderSize = 0;
+            this.masa13.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.masa13.ImageKey = "pngwing.com (2).png";
+            this.masa13.ImageList = this.ımageList1;
+            this.masa13.Location = new System.Drawing.Point(378, 146);
+            this.masa13.Name = "masa13";
+            this.masa13.Size = new System.Drawing.Size(116, 116);
+            this.masa13.TabIndex = 12;
+            this.masa13.Text = "Masa-13";
+            this.masa13.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.masa13.UseVisualStyleBackColor = true;
+            // 
+            // masa12
+            // 
+            this.masa12.FlatAppearance.BorderSize = 0;
+            this.masa12.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.masa12.ImageKey = "pngwing.com (2).png";
+            this.masa12.ImageList = this.ımageList1;
+            this.masa12.Location = new System.Drawing.Point(256, 146);
+            this.masa12.Name = "masa12";
+            this.masa12.Size = new System.Drawing.Size(116, 116);
+            this.masa12.TabIndex = 11;
+            this.masa12.Text = "Masa-12";
+            this.masa12.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.masa12.UseVisualStyleBackColor = true;
+            // 
+            // masa11
+            // 
+            this.masa11.FlatAppearance.BorderSize = 0;
+            this.masa11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.masa11.ImageKey = "pngwing.com (2).png";
+            this.masa11.ImageList = this.ımageList1;
+            this.masa11.Location = new System.Drawing.Point(134, 146);
+            this.masa11.Name = "masa11";
+            this.masa11.Size = new System.Drawing.Size(116, 116);
+            this.masa11.TabIndex = 10;
+            this.masa11.Text = "Masa-11";
+            this.masa11.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.masa11.UseVisualStyleBackColor = true;
+            // 
+            // masa10
+            // 
+            this.masa10.FlatAppearance.BorderSize = 0;
+            this.masa10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.masa10.ImageKey = "pngwing.com (2).png";
+            this.masa10.ImageList = this.ımageList1;
+            this.masa10.Location = new System.Drawing.Point(12, 146);
+            this.masa10.Name = "masa10";
+            this.masa10.Size = new System.Drawing.Size(116, 116);
+            this.masa10.TabIndex = 9;
+            this.masa10.Text = "Masa-10";
+            this.masa10.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.masa10.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(1148, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(196, 27);
+            this.dateTimePicker1.TabIndex = 18;
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(1179, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 112);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "MASA AKTİFLEŞTİR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(1356, 554);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.masa18);
+            this.Controls.Add(this.masa17);
+            this.Controls.Add(this.masa16);
+            this.Controls.Add(this.masa15);
+            this.Controls.Add(this.masa14);
+            this.Controls.Add(this.masa13);
+            this.Controls.Add(this.masa12);
+            this.Controls.Add(this.masa11);
+            this.Controls.Add(this.masa10);
+            this.Controls.Add(this.masa9);
+            this.Controls.Add(this.masa8);
+            this.Controls.Add(this.masa7);
+            this.Controls.Add(this.masa6);
+            this.Controls.Add(this.masa5);
+            this.Controls.Add(this.masa4);
+            this.Controls.Add(this.masa3);
+            this.Controls.Add(this.masa2);
+            this.Controls.Add(this.masa1);
+            this.Name = "Form1";
+            this.Text = "A Internet Cafe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button masa1;
+        private Button masa2;
+        private Button masa3;
+        private Button masa4;
+        private Button masa5;
+        private Button masa6;
+        private Button masa7;
+        private Button masa8;
+        private Button masa9;
+        private Button masa18;
+        private Button masa17;
+        private Button masa16;
+        private Button masa15;
+        private Button masa14;
+        private Button masa13;
+        private Button masa12;
+        private Button masa11;
+        private Button masa10;
+        private ImageList ımageList1;
+        private DateTimePicker dateTimePicker1;
+        private Button button1;
+    }
+}
